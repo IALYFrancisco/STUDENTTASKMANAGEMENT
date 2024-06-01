c53a9ed8b1530dc159e2a596e154617b07450bf3
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2024-06-01 11:07:18 +0300

    personalisation de la page d'accueil

---
+++ resources/js/Components/ApplicationLogo.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="../../images/icons8-add-bookmark-512.png" alt="logo de STM" title="logo de STM">
+</template>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,13 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/Dashboard.css')}}">
+    <link rel="shortcut icon" href="{{asset('images/logo-64.png')}}" type="image/x-icon">
+    <title>Dashboard - STM</title>
+</head>
+<body>
+<h1>Hello</h1>
+</body>
+</html>
